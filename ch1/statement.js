f923c78f4e57a7073c332d21294feef154f341da
Vespucci95 <donglonk@naver.com>
2023-02-11 16:19:35 +0900

    - 변수 인라인하기 36p

--- ch1/statement.js
+++ ch1/statement.js
@@ -1,48 +1,46 @@
 const statement = (invoice, plays) => {
-  let totalAmount = 0 // 총액
-  let volumeCredits = 0 // 적립 포인트.
-  let result = `청구 내역 (고객명: ${invoice.customer})\n`
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format
+    let totalAmount = 0 // 총액
+    let volumeCredits = 0 // 적립 포인트.
+    let result = `청구 내역 (고객명: ${invoice.customer})\n`
+    const format = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+    }).format
 
-  const playFor = (aPerformance) => plays[aPerformance.playID];
+    const playFor = (aPerformance) => plays[aPerformance.playID];
 
-  const amountFor = (play, aPerformance) => {
-    let result = 0
-    // const play = playFor(aPerformance);
-    switch (play.type) {
-      case 'tragedy': {
-        result = 40000
-        if (aPerformance.audience > 30) result += 1000 * (aPerformance.audience - 30)
-        break
-      }
-      case 'comedy': {
-        result = 30000
-        if (aPerformance.audience > 20) result += 10000 + 500 * (aPerformance.audience - 20)
-        result += 300 * aPerformance.audience
-        break
-      }
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`)
+    const amountFor = (aPerformance, play) => {
+        let result = 0
+        switch (play.type) {
+            case 'tragedy': {
+                result = 40000
+                if (aPerformance.audience > 30) result += 1000 * (aPerformance.audience - 30)
+                break
+            }
+            case 'comedy': {
+                result = 30000
+                if (aPerformance.audience > 20) result += 10000 + 500 * (aPerformance.audience - 20)
+                result += 300 * aPerformance.audience
+                break
+            }
+            default:
+                throw new Error(`알 수 없는 장르: ${play.type}`)
+        }
+        return result;
     }
-    return result;
-  }
 
-  for (let perf of invoice.performances) {
-    const play = playFor(perf);
-    let thisAmount = amountFor(play, perf);
+    for (let perf of invoice.performances) {
+        let thisAmount = amountFor(perf, playFor(perf));
 
-    volumeCredits += Math.max(perf.audience - 30, 0)
-    if (play.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5)
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`
-    totalAmount += thisAmount
-  }
-  result += `총액: ${format(totalAmount / 100)}\n`
-  result += `적립 포인트: ${volumeCredits}점\n`
-  return result
+        volumeCredits += Math.max(perf.audience - 30, 0)
+        if (playFor(perf).type === 'comedy') volumeCredits += Math.floor(perf.audience / 5)
+        result += `  ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`
+        totalAmount += thisAmount
+    }
+    result += `총액: ${format(totalAmount / 100)}\n`
+    result += `적립 포인트: ${volumeCredits}점\n`
+    return result
 }
 
 export default statement
