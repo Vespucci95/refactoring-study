8ed9808db9640448d99ef18e022ba9247249f39d
Vespucci95 <donglonk@naver.com>
2023-02-11 18:08:57 +0900

    - 파일 분리 html

--- ch1/statement.js
+++ ch1/statement.js
@@ -1,12 +1,12 @@
 import createStatementData from "./createStatementData.js";
 
-const renderPlainText = (data, plays) => {
-    const usd = aNumber => new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-        minimumFractionDigits: 2,
-    }).format(aNumber / 100);
+const usd = aNumber => new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+}).format(aNumber / 100);
 
+const renderPlainText = (data) => {
     let result = `청구 내역 (고객명: ${data.customer})\n`
     for (let perf of data.performances) {
         result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`
@@ -17,8 +17,35 @@ const renderPlainText = (data, plays) => {
     return result
 }
 
-const statement = (invoice, plays) => {
-    return renderPlainText(createStatementData(invoice, plays), plays);
+const renderHtml = (data) => {
+    let result = `
+<h1>청구 내역 (고객명: ${data.customer})</h1>
+<table>
+<tr><th>연극</th><th>좌석수</th><th>금액</th></tr>
+`
+    for (let perf of data.performances) {
+        result += `
+<tr>
+<td>${perf.play.name}</td>
+<td>${perf.audience}</td>
+<td>${usd(perf.amount)}</td>
+</tr>
+`
+    }
+
+    result += `</table>
+<p>총액: <em>${usd(data.totalAmount)}</em></p>
+<p>적립 포인트: <em>${data.totalVolumeCredits}점</em></p>
+`
+
+    return result
+}
+
+export const statement = (invoice, plays) => {
+    return renderPlainText(createStatementData(invoice, plays));
+}
+export const htmlStatement = (invoice, plays) => {
+    return renderHtml(createStatementData(invoice, plays));
 }
 
 export default statement;
