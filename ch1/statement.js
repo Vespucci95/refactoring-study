78e4cc13116a11efb07f35f39b5169e074c76337
Vespucci95 <donglonk@naver.com>
2023-02-11 16:59:49 +0900

    - 함수 추출하기 / 변수를 인라인 하기 46p

--- ch1/statement.js
+++ ch1/statement.js
@@ -34,23 +34,26 @@ const statement = (invoice, plays) => {
         style: 'currency',
         currency: 'USD',
         minimumFractionDigits: 2,
-    }).format(aNumber/100);
+    }).format(aNumber / 100);
 
-    let totalAmount = 0 // 총액
+    const totalVolumeCredits = () => {
+        let result = 0 // 적립 포인트.
+        for (let perf of invoice.performances) {
+            result += volumeCreditsFor(perf);
+        }
+        return result;
+    }
+
     let result = `청구 내역 (고객명: ${invoice.customer})\n`
 
+    let totalAmount = 0 // 총액
     for (let perf of invoice.performances) {
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
         totalAmount += amountFor(perf);
     }
 
-    let volumeCredits = 0 // 적립 포인트.
-    for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-    }
-
     result += `총액: ${usd(totalAmount)}\n`
-    result += `적립 포인트: ${volumeCredits}점\n`
+    result += `적립 포인트: ${totalVolumeCredits()}점\n`
     return result
 }
 
