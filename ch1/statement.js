9e6b17ea95ea60068ae32130499b480eb0e4d4d2
Vespucci95 <donglonk@naver.com>
2023-02-11 16:01:43 +0900

    - 함수 추출하기 33p

--- ch1/statement.js
+++ ch1/statement.js
@@ -1,6 +1,6 @@
 const statement = (invoice, plays) => {
-  let totalAmount = 0
-  let volumeCredits = 0
+  let totalAmount = 0 // 총액
+  let volumeCredits = 0 // 적립 포인트.
   let result = `청구 내역 (고객명: ${invoice.customer})\n`
   const format = new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -8,25 +8,30 @@ const statement = (invoice, plays) => {
     minimumFractionDigits: 2,
   }).format
 
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID]
-    let thisAmount = 0
+  const amountFor = (play, perf) => {
+    let result = 0
 
     switch (play.type) {
       case 'tragedy': {
-        thisAmount = 40000
-        if (perf.audience > 30) thisAmount += 1000 * (perf.audience - 30)
+        result = 40000
+        if (perf.audience > 30) result += 1000 * (perf.audience - 30)
         break
       }
       case 'comedy': {
-        thisAmount = 30000
-        if (perf.audience > 20) thisAmount += 10000 + 500 * (perf.audience - 20)
-        thisAmount += 300 * perf.audience
+        result = 30000
+        if (perf.audience > 20) result += 10000 + 500 * (perf.audience - 20)
+        result += 300 * perf.audience
         break
       }
       default:
         throw new Error(`알 수 없는 장르: ${play.type}`)
     }
+    return result;
+  }
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID]
+    let thisAmount = amountFor(play, perf);
 
     volumeCredits += Math.max(perf.audience - 30, 0)
     if (play.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5)
