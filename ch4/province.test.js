74d094f1c7a311a4128d1e7f0e4ad024009b4ac8
hyunseo-common <donglonk@naver.com>
2023-02-28 16:44:27 +0900

    - TEST 작성 해보기

--- ch4/province.test.js
+++ ch4/province.test.js
@@ -1,12 +1,21 @@
 import Province from './province.js'
+import {describe, it} from "mocha"
+import {assert} from 'chai'
 
 const sampleProvinceData = () => ({
-  name: 'Asia',
-  producers: [
-    { name: 'Byzzantium', cost: 10, production: 9 },
-    { name: 'Attalia', cost: 12, production: 10 },
-    { name: 'Sinope', cost: 10, production: 6 },
-  ],
-  demand: 30,
-  price: 20,
+    name: 'Asia',
+    producers: [
+        {name: 'Byzzantium', cost: 10, production: 9},
+        {name: 'Attalia', cost: 12, production: 10},
+        {name: 'Sinope', cost: 10, production: 6},
+    ],
+    demand: 30,
+    price: 20,
+})
+
+describe("province", () => {
+    it('shortfall', () => {
+        const asia = new Province(sampleProvinceData());
+        assert.equal(asia.shortfall, 5);
+    })
 })
