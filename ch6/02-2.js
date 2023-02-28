d2efec0772f986a8b145b77fc2f27f3c508cb503
hyunseo-common <donglonk@naver.com>
2023-02-28 17:39:39 +0900

    - ch06 02-2 refactoring

--- ch6/02-2.js
+++ ch6/02-2.js
@@ -1,11 +1,8 @@
-const gatherCustomerData = (out, aCustomer) => {
-  out.push(['name', aCustomer.name])
-  out.push(['location', aCustomer.location])
-}
 const reportLines = aCustomer => {
-  const lines = []
-  gatherCustomerData(lines, aCustomer)
-  return lines
+  const result = []
+  result.push(['name', aCustomer.name])
+  result.push(['location', aCustomer.location])
+  return result
 }
 
 const customerA = { name: 'roy', location: 'seoul' }
