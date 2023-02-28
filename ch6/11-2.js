8950e3fe98baee03118fc8c0349d5ea891456be6
hyunseo-common <donglonk@naver.com>
2023-02-28 19:53:49 +0900

    - ch06 11-2 refactoring

--- ch6/11-2.js
+++ ch6/11-2.js
@@ -8,25 +8,37 @@ class Order {
     }
 }
 
-const main = () => {
-    try {
-        const argv = process.argv
-        if (argv.length < 3) throw new Error('파일명을 입력하세요')
-        const filename = argv[argv.length - 1]
-        const input = readJSON(filename)
-        const orders = input.map(item => new Order(item))
+class CommandLine {}
 
-        if (argv.includes('-r')) {
-            const readyOrders = orders.filter(o => o.product.status === 'ready')
-            console.log('ready', readyOrders.length)
-        } else {
-            console.log('not ready', orders.length)
-        }
+const countOrders = (commandLine, args, filename) => {
+    const input = readJSON(filename)
+    const orders = input.map(item => new Order(item))
+
+    if (args.includes('-r')) {
+        const readyOrders = orders.filter(o => o.product.status === 'ready')
+        return readyOrders.length
+    } else {
+        return orders.length
+    }
+}
+
+const run = args => {
+    if (args.length === 0) {
+        throw new Error('파일명을 입력하세요')
+    }
+    const commandLine = new CommandLine();
+    const filename = args[args.length - 1]
+    return countOrders(commandLine, args, filename);
+}
+
+const main = (args) => {
+    try {
+        console.log(run(args));
     } catch (err) {
         console.error(err)
     }
 }
-main()
+main(process.argv.slice(2))
 
 // node ch6/11-2.js -r ch6/11-products.json
 // node ch6/11-2.js ch6/11-products.json
