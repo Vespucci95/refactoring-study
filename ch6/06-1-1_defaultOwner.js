8c494e6196bbc627e65fedd1d7db0655f4416986
hyunseo-common <donglonk@naver.com>
2023-02-28 18:35:30 +0900

    - ch06 06-2 refactoring

--- ch6/06-1-1_defaultOwner.js
+++ ch6/06-1-1_defaultOwner.js
@@ -1,6 +1,15 @@
+import {assert} from "chai";
+
 let defaultOwnerData = { firstName: '마틴', lastName: '파울러' }
 
 export const defaultOwner = () => ({ ...defaultOwnerData })
-export const sertDefaultOwner = arg => {
+export const setDefaultOwner = arg => {
   defaultOwnerData = arg
 }
+
+const owner1 = defaultOwner();
+assert.equal('파울러', owner1.lastName, '처음값 확인')
+
+const owner2 = defaultOwner();
+owner2.lastName = '파슨스'
+assert.equal('파슨스', owner1.lastName, 'owner2 변경 후')
