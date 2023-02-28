c0b85940270c4a0eeddeb0de987d987a32bcbdab
hyunseo-common <donglonk@naver.com>
2023-02-28 18:28:55 +0900

    - ch06 06-1 refactoring

--- ch6/06-1-0.js
+++ ch6/06-1-0.js
@@ -1,12 +1,15 @@
-let defaultOwner = { firstName: '마틴', lastName: '파울러' }
+let defaultOwnerData = {firstName: '마틴', lastName: '파울러'}
+
+const defaultOwner = () => defaultOwnerData
+
+const setDefaultOwner = arg => {
+    defaultOwnerData = arg
+}
 
 const spaceship = {
-  owner: defaultOwner,
+    owner: defaultOwner(),
 }
 
-defaultOwner = { firstName: '레베카', lastName: '파슨스' }
+setDefaultOwner({firstName: '레베카', lastName: '파슨스'})
 
-const getDefaultOwner = () => defaultOwner
-const sertDefaultOwner = arg => {
-  defaultOwner = arg
-}
+console.log(spaceship, defaultOwner)
