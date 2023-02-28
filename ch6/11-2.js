647bf4b7c0e284a54c49909d7dd31b1eb3978aeb
hyunseo-common <donglonk@naver.com>
2023-02-28 19:48:02 +0900

    - ch06 11-2 refactoring start

--- ch6/11-2.js
+++ ch6/11-2.js
@@ -1,29 +1,32 @@
-import { readJSON } from '../fileController.js'
+import {readJSON} from '../fileController.js'
 
-console.log(process.argv)
 class Order {
-  product = {}
-  constructor(product) {
-    this.product = product
-  }
+    product = {}
+
+    constructor(product) {
+        this.product = product
+    }
 }
 
 const main = () => {
-  try {
-    const argv = process.argv
-    if (argv.length < 3) throw new Error('파일명을 입력하세요')
-    const filename = argv[argv.length - 1]
-    const input = readJSON(filename)
-    const orders = input.map(item => new Order(item))
+    try {
+        const argv = process.argv
+        if (argv.length < 3) throw new Error('파일명을 입력하세요')
+        const filename = argv[argv.length - 1]
+        const input = readJSON(filename)
+        const orders = input.map(item => new Order(item))
 
-    if (argv.includes('-r')) {
-      const readyOrders = orders.filter(o => o.product.status === 'ready')
-      console.log('ready', readyOrders.length)
-    } else {
-      console.log('not ready', orders.length)
+        if (argv.includes('-r')) {
+            const readyOrders = orders.filter(o => o.product.status === 'ready')
+            console.log('ready', readyOrders.length)
+        } else {
+            console.log('not ready', orders.length)
+        }
+    } catch (err) {
+        console.error(err)
     }
-  } catch (err) {
-    console.error(err)
-  }
 }
 main()
+
+// node ch6/11-2.js -r ch6/11-products.json
+// node ch6/11-2.js ch6/11-products.json
