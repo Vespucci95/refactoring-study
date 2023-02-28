ee4bdea084708d9a8e48bbdf8dc3b0e13a33b0d6
hyunseo-common <donglonk@naver.com>
2023-02-28 17:47:06 +0900

    - ch06 03-2 refactoring

--- ch6/03-2.js
+++ ch6/03-2.js
@@ -1,29 +1,40 @@
 class Order {
-  constructor(aRecord) {
-    this._data = aRecord
-  }
-  get quantity() {
-    return this._data.quantity
-  }
-  get itemPrice() {
-    return this._data.itemPrice
-  }
-  get price() {
-    return (
-      this.quantity * this.itemPrice -
-      Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
-      Math.min(this.quantity * this.itemPrice * 0.1, 100)
-    )
-  }
+    constructor(aRecord) {
+        this._data = aRecord
+    }
+
+    get quantity() {
+        return this._data.quantity
+    }
+
+    get itemPrice() {
+        return this._data.itemPrice
+    }
+
+    get basePrice() {
+        return this.quantity * this.itemPrice
+    }
+
+    get quantityDiscount() {
+        return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05
+    }
+
+    get shipping() {
+        return Math.min(this.quantity * this.itemPrice * 0.1, 100)
+    }
+
+    get price() {
+        return this.basePrice - this.quantityDiscount + this.shipping
+    }
 }
 
 const orderA = new Order({
-  itemPrice: 600,
-  quantity: 3,
+    itemPrice: 600,
+    quantity: 3,
 })
 const orderB = new Order({
-  itemPrice: 8000,
-  quantity: 2,
+    itemPrice: 8000,
+    quantity: 2,
 })
 
 console.log(orderA.price)
