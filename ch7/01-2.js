8d3da5e89fa06c277e257f4378f908e7d2a44a35
Vespucci95 <donglonk@naver.com>
2023-03-12 19:01:40 +0900

    242p

--- ch7/01-2.js
+++ ch7/01-2.js
@@ -1,19 +1,27 @@
-import { readJSON } from '../fileController.js'
+import {readJSON} from '../fileController.js'
 
-let customerData = readJSON('ch7/01-2.json')
 
-const getRawDataOfCustomers = () => customerData;
+class CustomerData {
+    constructor(data) {
+        this._data = data;
+    }
+}
+
+let customerData = new CustomerData(readJSON('ch7/01-2.json'))
+
+const getCustomerData = () => customerData;
+export const getRawDataOfCustomers = () => customerData._data;
 const setRawDataOfCustomers = (arg) => {
-  customerData = arg
+    customerData = new CustomerData(arg);
 };
 
 export const writeData = (customerId, year, month, amount) => {
-  getRawDataOfCustomers()[customerId].usages[year][month] = amount
+    getRawDataOfCustomers()[customerId].usages[year][month] = amount
 }
 
 export const compareUsage = (customerId, laterYear, month) => {
-  const later = getRawDataOfCustomers()[customerId].usages[laterYear][month]
-  const earlier = getRawDataOfCustomers()[customerId].usages[laterYear - 1][month]
-  return { laterAmount: later, change: later - earlier }
+    const later = getRawDataOfCustomers()[customerId].usages[laterYear][month]
+    const earlier = getRawDataOfCustomers()[customerId].usages[laterYear - 1][month]
+    return {laterAmount: later, change: later - earlier}
 }
 export const getCustomer = () => customerData
