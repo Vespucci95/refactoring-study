904cbaf59f037b3fee2e49ba53dc4a1e2ea75473
Vespucci95 <donglonk@naver.com>
2023-03-12 18:07:47 +0900

    - 상수를 캡슐화하기

--- ch7/01-1.js
+++ ch7/01-1.js
@@ -1,7 +1,10 @@
 let result = ''
 const organization = { name: '애크미 구스베리', country: 'GB' }
+const getRawDataOfOrganization = () => {
+    return organization;
+}
 
-organization.name = '애그머니 블루베리'
-result += `<h1>${organization.name}</h1>`
+getRawDataOfOrganization().name = '애그머니 블루베리'
+result += `<h1>${getRawDataOfOrganization().name}</h1>`
 
 console.log(result)
