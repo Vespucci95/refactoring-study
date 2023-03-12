689d8ab63830c90d7f326df41c53128210240517
Vespucci95 <donglonk@naver.com>
2023-03-12 18:21:35 +0900

    240p

--- ch7/01-1.js
+++ ch7/01-1.js
@@ -1,10 +1,23 @@
-let result = ''
-const organization = { name: '애크미 구스베리', country: 'GB' }
-const getRawDataOfOrganization = () => {
-    return organization;
+class Organization {
+    constructor(data) {
+        this._data = data;
+    }
+
+    get name() {
+        return this._data.name;
+    }
+
+    set name(aString) {
+        this._data.name = aString;
+    }
 }
 
-getRawDataOfOrganization().name = '애그머니 블루베리'
-result += `<h1>${getRawDataOfOrganization().name}</h1>`
+
+let result = ''
+const organization = new Organization({name: '애크미 구스베리', country: 'GB'});
+const getOrganization = () => organization;
+
+getOrganization().name = '애그머니 블루베리'
+result += `<h1>${getOrganization().name}</h1>`
 
 console.log(result)
