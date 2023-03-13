0227a069060b671587e4b54aa64b39c3781ea2af
Vespucci95 <donglonk@naver.com>
2023-03-14 00:46:47 +0900

    275p

--- ch7/09.js
+++ ch7/09.js
@@ -1,9 +1,5 @@
 const findPerson = people => {
-  for (let i = 0; i < people.length; i++) {
-    if (people[i] === 'Don') return 'Don'
-    if (people[i] === 'John') return 'John'
-    if (people[i] === 'Kent') return 'Kent'
-  }
-  return ''
+  const candidates = ["Don", "John", "Kent"]
+  return people.find(p => candidates.includes(p)) || ""
 }
 console.log(findPerson(['Roy', 'Jay', 'Don', 'Kay', 'John', 'Peter', 'Kent', 'Clark']))
