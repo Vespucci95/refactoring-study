d3aaef4c275dc0ec7de4baf4f0e678ca4c4f6ff8
Vespucci95 <donglonk@naver.com>
2023-03-13 23:56:48 +0900

    255p

--- ch7/03.js
+++ ch7/03.js
@@ -1,14 +1,54 @@
-class Order {
-  priority = ''
-  constructor(data) {
-    this.priority = data.priority
-  }
+class Priority {
+    static legalValues() {
+        return ['low', 'normal', 'high', 'rush'];
+    }
+
+    constructor(value) {
+        if (value instanceof Priority) return value;
+        if (Priority.legalValues().includes(value)) {
+            this._value = value;
+        } else {
+            throw new Error(`<${value}>는 유효하지 않는 우선순위 입니다.`)
+        }
+        this._value = value;
+    }
+
+    toString() {
+        return this._value;
+    }
+    get _index() {
+        return Priority.legalValues().findIndex(s => s === this._value);
+    }
+    equals(other) { return this._index === other._index }
+    higherThan(other) { return this._index > other._index }
+    lowerThan(other) { return this._index < other._index }
 }
+
+class Order {
+    _priority
+
+    constructor(data) {
+        this._priority = new Priority(data.priority);
+    }
+
+    get priority() {
+        return this._priority;
+    }
+
+    get priorityString() {
+        return this._priority.toString();
+    }
+
+    set priority(aString) {
+        this._priority = new Priority(aString);
+    }
+}
+
 const client1 = () => {
-  const orders = [{ priority: 'high' }, { priority: 'rush' }, { priority: 'low' }, { priority: 'normal' }].map(
-    o => new Order(o),
-  )
-  const highPriorityCount = orders.filter(o => o.priority === 'high' || o.priority === 'rush').length
-  return highPriorityCount
+    const orders = [{priority: 'high'}, {priority: 'rush'}, {priority: 'low'}, {priority: 'normal'}].map(
+        o => new Order(o),
+    )
+    const highPriorityCount = orders.filter(o => o.priority.higherThan(new Priority('normal'))).length
+    return highPriorityCount
 }
 console.log(client1())
