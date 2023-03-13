9c6fa9f6ade39374ecf94f187284fddc402dd420
Vespucci95 <donglonk@naver.com>
2023-03-14 00:11:36 +0900

    259p

--- ch7/04.js
+++ ch7/04.js
@@ -5,12 +5,20 @@ class Order {
     this._quantity = quantity
     this._item = item
   }
-  get price() {
-    let basePrice = this._quantity * this._item.price
-    let discountFactor = 0.98
-    if (basePrice > 1000) discountFactor -= 0.03
-    return basePrice * discountFactor
+  get basePrice() {
+    return this._quantity * this._item.price;
   }
+
+  get discountFactor() {
+    let discountFactor = 0.98
+    if (this.basePrice > 1000) discountFactor -= 0.03
+    return discountFactor;
+  }
+  get price() {
+    let discountFactor = this.discountFactor;
+    return this.basePrice * discountFactor
+  }
+
 }
 const order = new Order(10, { price: 1000 })
 console.log(order.price)
