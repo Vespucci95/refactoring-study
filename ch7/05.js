9c768a7eaf8bebda3b8d81b97dea143ba2d7cc86
Vespucci95 <donglonk@naver.com>
2023-03-14 00:19:56 +0900

    263p

--- ch7/05.js
+++ ch7/05.js
@@ -1,23 +1,32 @@
+class TelephoneNumber {
+  get areaCode() {
+    return this._areaCode
+  }
+  set areaCode(arg) {
+    this._areaCode = arg
+  }
+  get number() {
+    return this._number
+  }
+  set number(arg) {
+    this._number = arg
+  }
+  toString() {
+    return `(${this.areaCode}) ${this.number}`
+  }
+}
+
 class Person {
+  constructor() {
+    this._telephoneNumber = new TelephoneNumber();
+  }
   get name() {
     return this._name
   }
   set name(arg) {
     this._name = arg
   }
-  get officeAreaCode() {
-    return this._officeAreaCode
-  }
-  set officeAreaCode(arg) {
-    this._officeAreaCode = arg
-  }
-  get officeNumber() {
-    return this._officeNumber
-  }
-  set officeNumber(arg) {
-    this._officeNumber = arg
-  }
   get telephoneNumber() {
-    return `(${this.officeAreaCode}) ${this.officeNumber}`
+    this._telephoneNumber.toString()
   }
 }
