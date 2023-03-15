77e73b1a53c827c69504e3c7a1890738ef273649
Vespucci95 <donglonk@naver.com>
2023-03-16 07:28:09 +0900

    315p

--- ch8/06-1.js
+++ ch8/06-1.js
@@ -1,12 +1,12 @@
 const retreivePricingPlan = () => ({
-  base: 50, // 기본요금
-  unit: 100, // 제품 하나당 가격
-  discountThreshold: 7, // 몇개 이상부터 할인 적용할지 (8개: 0개 할인. 8개: 1개 할인. 10개: 3개 할인)
-  discountFactor: 20, // 개당 할인액
+    base: 50, // 기본요금
+    unit: 100, // 제품 하나당 가격
+    discountThreshold: 7, // 몇개 이상부터 할인 적용할지 (8개: 0개 할인. 8개: 1개 할인. 10개: 3개 할인)
+    discountFactor: 20, // 개당 할인액
 })
 const retreiveOrder = () => ({
-  units: 8,
-  isRepeat: true, // 반복주문시 추가할인
+    units: 8,
+    isRepeat: true, // 반복주문시 추가할인
 })
 const chargeOrder = charge => console.log(charge)
 
@@ -16,16 +16,8 @@ const chargeOrder = charge => console.log(charge)
 // 반복주문에 대한 추가할인액 20
 // result: 810
 
-const pricingPlan = retreivePricingPlan()
-const order = retreiveOrder()
-const baseCharge = pricingPlan.base
-let charge
-const chargePerUnit = pricingPlan.unit
-const units = order.units
-let discount
-charge = baseCharge + units * chargePerUnit
-let discountableUnits = Math.max(units - pricingPlan.discountThreshold, 0)
-discount = discountableUnits * pricingPlan.discountFactor
-if (order.isRepeat) discount += 20
-charge = charge - discount
-chargeOrder(charge)
+const {discountFactor, base: baseCharge, discountThreshold, unit: chargePerUnit} = retreivePricingPlan()
+const {units, isRepeat} = retreiveOrder()
+const discountableUnits = Math.max(units - discountThreshold, 0)
+const discount = discountableUnits * discountFactor + (isRepeat ? 20 : 0)
+chargeOrder(baseCharge + units * chargePerUnit - discount)
