101d225e17bc7c75bd404fa62c3c4f25ee590682
Vespucci95 <donglonk@naver.com>
2023-03-16 06:38:36 +0900

    295p

--- ch8/02-2.js
+++ ch8/02-2.js
@@ -1,22 +1,26 @@
+import assert from 'assert';
 class Account {
   _number
   _type
-  _interestRate
-  constructor(number, type, interestRate) {
+  constructor(number, type) {
     this._number = number
     this._type = type
-    this._interestRate = interestRate
   }
   get interestRate() {
-    return this._interestRate
+    return this._type.interestRate;
   }
 }
 class AccountType {
   _name
-  constructor(nameString) {
+  _interestRate
+  constructor(nameString, interestRate) {
     this._name = nameString
+    this._interestRate = interestRate;
+  }
+  get interestRate() {
+    return this._interestRate;
   }
 }
-const minus = new AccountType('마통')
-const acc = new Account(100000, minus, 0.39)
+const minus = new AccountType('마통', 0.39)
+const acc = new Account(100000, minus)
 console.log(acc.interestRate)
