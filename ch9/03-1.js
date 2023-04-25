344698911e887a6cb5f94b61dc318f9ddd2d40c8
Vespucci95 <donglonk@naver.com>
2023-04-25 23:06:48 +0900

    340p

--- ch9/03-1.js
+++ ch9/03-1.js
@@ -1,12 +1,10 @@
 class ProductionPlan {
-  #production = 0
   #adjustments = []
   get production() {
-    return this.#production
+    return this.#adjustments.reduce((sum, a) => sum + a.amount, 0);
   }
   applyAdjustment(anAdjustment) {
     this.#adjustments.push(anAdjustment)
-    this.#production += anAdjustment.amount
   }
 }
 
