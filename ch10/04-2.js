80ba04ae8612519b5ff7e0e2a8a75b8bef9512fe
Vespucci95 <donglonk@naver.com>
2023-05-07 00:28:32 +0900

    387p

--- ch10/04-2.js
+++ ch10/04-2.js
@@ -35,20 +35,17 @@ class Rating {
         let result = 2
         if (this.voyage.zone === '중국') result += 1
         if (this.voyage.zone === '동인도') result += 1
-
-        result += this.voyageAndHistoryLengthFactor;
+        result += this.historyLengthFactor;
+        result += this.voyageLengthFactor;
         return result
     }
 
-    get voyageAndHistoryLengthFactor() {
-        let result = 0;
-        if (this.history.length > 8) result += 1
-        if (this.voyage.length > 14) result -= 1
-        return result;
+    get voyageLengthFactor() {
+        return (this.voyage.length > 14) ? -1 : 0
     }
 
-    get hasChina() {
-        return this.history.some(v => v.zone === '중국')
+    get historyLengthFactor() {
+        return (this.history.length > 8) ? 1 : 0;
     }
 }
 
@@ -58,9 +55,16 @@ class ExperiencedChinaRating extends Rating {
         return Math.max(result, 0)
     }
 
-    get voyageAndHistoryLengthFactor() {
-        let result = 3;
-        if (this.history.length > 10) result += 1
+    get historyLengthFactor() {
+        return (this.history.length > 10) ? 1 : 0;
+    }
+
+    get voyageProfitFactor() {
+        return super.voyageLengthFactor + 3;
+    }
+
+    get voyageLengthFactor() {
+        let result = 0;
         if (this.voyage.length > 12) result += 1
         if (this.voyage.length > 18) result -= 1
         return result;
