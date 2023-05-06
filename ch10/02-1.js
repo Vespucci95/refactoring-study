af3c8623de8cd00471088ab35656ced2634989bb
Vespucci95 <donglonk@naver.com>
2023-05-06 22:43:02 +0900

    358p

--- ch10/02-1.js
+++ ch10/02-1.js
@@ -1,6 +1,8 @@
+const isNotEligibleForDisability = anEmployee => {
+  if (anEmployee.seniority < 2 || anEmployee.monthsDisabled > 12 || anEmployee.isPartTime) return 0
+}
+
 const disabilityAmount = anEmployee => {
-  if (anEmployee.seniority < 2) return 0
-  if (anEmployee.monthsDisabled > 12) return 0
-  if (anEmployee.isPartTime) return 0
+  return isNotEligibleForDisability(anEmployee);
   // 장애 수당 계산
 }
