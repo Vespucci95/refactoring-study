8bb5b85af6b75e0cfe744fa1972d45cbfce240ea
Vespucci95 <donglonk@naver.com>
2023-05-20 13:53:52 +0900

    410p +

--- ch10/07.js
+++ ch10/07.js
@@ -1,5 +1,5 @@
 const sendAlert = () => console.warn('악당을 찾았소')
-
-const checkForMiscreants = people => people.some(p => ["조커", "사루만"].includes(p)) && sendAlert();
+const VILLAIN = ["조커", "사루만"];
+const checkForMiscreants = people => people.some(p => VILLAIN.includes(p)) && sendAlert();
 checkForMiscreants(['슈퍼맨', '배트맨', '아이언맨', '사루만', '블랙위도우', '조커', '스파이더맨'])
 checkForMiscreants(['슈퍼맨', '배트맨', '아이언맨', '블랙위도우', '스파이더맨'])
