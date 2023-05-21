c14ea035ea78c2003218ad8e676c35f4bf0e7d10
Vespucci95 <donglonk@naver.com>
2023-05-21 23:00:53 +0900

    452p

--- ch11/09.js
+++ ch11/09.js
@@ -1,20 +1,33 @@
-const score = (candidate, medicalExam, scoringGuide) => {
-  let result = 0
-  let healthLevel = 0
-  let highMedicalRiskFlag = false
-  if (medicalExam.isSmoker) {
-    healthLevel += 10
-    highMedicalRiskFlag = true
-  }
-  let certificationGrade = 'regular'
-  if (scoringGuide.stateWithLowCertification(candidate.originState)) {
-    certificationGrade = 'low'
-    result -= 5
-  }
-  result -= Math.max(healthLevel - 5, 0)
-  return { result, certificationGrade, highMedicalRiskFlag }
+const score = (candidate, medicalExam, scoringGuide) => new Score(candidate, medicalExam, scoringGuide).exec();
+
+class Score {
+    #candidate
+    #medicalExam
+    #scoringGuide
+    constructor(candidate, medicalExam, scoringGuide) {
+        this.#candidate = candidate
+        this.#medicalExam = medicalExam
+        this.#scoringGuide = scoringGuide
+    }
+
+    exec() {
+        let result = 0
+        let healthLevel = 0
+        let highMedicalRiskFlag = false
+        if (this.#medicalExam.isSmoker) {
+            healthLevel += 10
+            highMedicalRiskFlag = true
+        }
+        let certificationGrade = 'regular'
+        if (this.#scoringGuide.stateWithLowCertification(this.#candidate.originState)) {
+            certificationGrade = 'low'
+            result -= 5
+        }
+        result -= Math.max(healthLevel - 5, 0)
+        return {result, certificationGrade, highMedicalRiskFlag}
+    }
 }
 
-const scoringGuide = { stateWithLowCertification: state => state === 'CA' || state === 'ME' }
-console.log(score({ originState: 'CA' }, { isSmoker: true }, scoringGuide))
-console.log(score({ originState: 'NY' }, { isSmoker: false }, scoringGuide))
+const scoringGuide = {stateWithLowCertification: state => state === 'CA' || state === 'ME'}
+console.log(score({originState: 'CA'}, {isSmoker: true}, scoringGuide))
+console.log(score({originState: 'NY'}, {isSmoker: false}, scoringGuide))
