617cbe9efd91ffb529ec80eeb195496c64d17db0
Vespucci95 <donglonk@naver.com>
2023-05-21 23:21:05 +0900

    464p

--- ch11/11.js
+++ ch11/11.js
@@ -1,17 +1,18 @@
 const calculateAscent = () => {
+  let result = 0;
   for (let i = 1; i < points.length; i++) {
     const verticalChange = points[i].elevation - points[i - 1].elevation
-    totalAscent += verticalChange > 0 ? verticalChange : 0
+    result += verticalChange > 0 ? verticalChange : 0
   }
+  return result;
 }
 const calculateTime = () => {}
 const calculateDistance = () => {}
 
 let points = []
-let totalAscent = 0
-let totalTime = 0
-let totalDistance = 0
-calculateAscent()
+const totalAscent = calculateAscent()
+const totalTime = calculateTime()
+const totalDistance = calculateDistance()
 calculateTime()
 calculateDistance()
 const pace = totalTime / 60 / totalDistance
