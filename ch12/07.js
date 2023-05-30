bc90e2c8d303e98cf3e6d44cf7cda00a49b6a0d0
Vespucci95 <donglonk@naver.com>
2023-05-30 23:41:53 +0900

    498p

--- ch12/07.js
+++ ch12/07.js
@@ -21,6 +21,29 @@ class Female extends Person {
   }
 }
 
+const createPerson = name => new Person(name)
+const createMale = name => new Male(name)
+const createFemale = name => new Female(name)
+
+const loadFromInput = data => {
+  const result = [];
+  data.forEach(aRecord => {
+    let p;
+    switch (aRecord.gender) {
+        case 'M':
+            p = new Male(aRecord.name)
+            break
+        case 'F':
+            p = new Female(aRecord.name)
+            break
+        default:
+            p = new Person(aRecord.name)
+    }
+    result.push(p);
+  });
+  return result;
+}
+
 const people = [new Male('재남'), new Female('지금'), new Male('로이'), new Female('이지')]
 const numberOfMales = people.filter(p => p instanceof Male).length
 console.log(numberOfMales)
