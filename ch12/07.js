c93a5ac4bbe4a1f5c817c3d9ca42c973857edf13
Vespucci95 <donglonk@naver.com>
2023-05-30 23:48:29 +0900

    499p

--- ch12/07.js
+++ ch12/07.js
@@ -3,6 +3,9 @@ class Person {
   constructor(name) {
     this.#name = name
   }
+  isMale() {
+    return this instanceof Male;
+  }
   get name() {
     return this.#name
   }
@@ -21,29 +24,28 @@ class Female extends Person {
   }
 }
 
-const createPerson = name => new Person(name)
-const createMale = name => new Male(name)
-const createFemale = name => new Female(name)
+// const createPerson = name => new Person(name)
+// const createMale = name => new Male(name)
+// const createFemale = name => new Female(name)
 
-const loadFromInput = data => {
-  const result = [];
-  data.forEach(aRecord => {
-    let p;
-    switch (aRecord.gender) {
-        case 'M':
-            p = new Male(aRecord.name)
-            break
-        case 'F':
-            p = new Female(aRecord.name)
-            break
-        default:
-            p = new Person(aRecord.name)
-    }
-    result.push(p);
-  });
-  return result;
+const createPerson = aRecord => {
+  let p;
+  switch (aRecord.gender) {
+    case 'M':
+      p = new Male(aRecord.name)
+      break
+    case 'F':
+      p = new Female(aRecord.name)
+      break
+    default:
+      p = new Person(aRecord.name)
+  }
+  return p
 }
+const loadFromInput = data => data.map(aRecord => createPerson(aRecord));
 
 const people = [new Male('재남'), new Female('지금'), new Male('로이'), new Female('이지')]
-const numberOfMales = people.filter(p => p instanceof Male).length
+
+
+const numberOfMales = people.filter(p => p.isMale()).length
 console.log(numberOfMales)
