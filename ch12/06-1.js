a68708b87200fd44bb038da4c28ba31cce3144d4
Vespucci95 <donglonk@naver.com>
2023-05-30 23:06:46 +0900

    491p

--- ch12/06-1.js
+++ ch12/06-1.js
@@ -1,19 +1,47 @@
 class Employee {
   #name
   #type
-  constructor(name, type) {
-    this.validateType(type)
+  constructor(name) {
     this.#name = name
-    this.#type = type
   }
-  validateType(arg) {
-    if (!['engineer', 'manager', 'salesperson'].includes(arg)) throw new Error(`${arg}라는 직원 유형은 없습니다.`)
+  get type() {
+    return '';
   }
   toString() {
     return `${this.#name} is a ${this.#type}`
   }
 }
-const roy = new Employee('roy', 'engineer')
-const jay = new Employee('jay', 'manager')
-const kay = new Employee('kay', 'salesperson')
-const tei = new Employee('tei', 'nobody')
+
+class Engineer extends Employee {
+  get type() {
+    return 'engineer'
+  }
+}
+class SalesPerson extends Employee {
+  get type() {
+    return 'salesperson'
+  }
+}
+class Manager extends Employee {
+  get type() {
+    return 'manager'
+  }
+}
+
+const createEmployee = (name, type) => {
+    switch(type) {
+      case 'engineer':
+        return new Engineer(name)
+      case 'salesperson':
+        return new SalesPerson(name)
+      case 'manager':
+        return new Manager(name)
+      default:
+        throw new Error(`${type}라는 직원 유형은 없습니다.`)
+    }
+}
+
+const roy = createEmployee('roy', 'engineer')
+const jay = createEmployee('jay', 'manager')
+const kay = createEmployee('kay', 'salesperson')
+const tei = createEmployee('tei', 'nobody')
