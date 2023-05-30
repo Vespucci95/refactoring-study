fc0ced333c195eb142bc954c1c4e9c667035f8e2
Vespucci95 <donglonk@naver.com>
2023-05-30 22:33:03 +0900

    278p

--- ch12/01.js
+++ ch12/01.js
@@ -1,14 +1,19 @@
 class Party {
   monthlyCost
-}
-
-class Employee extends Party {
   get annualCost() {
     return this.monthlyCost * 12
   }
 }
-class Department extends Party {
-  get totalAnnualCost() {
-    return this.monthlyCost * 12
-  }
+
+class Employee extends Party {
+  monthlyCost = 10
 }
+class Department extends Party {
+  monthlyCost = 20
+}
+
+const e = new Employee()
+const d = new Department()
+
+console.log(e.annualCost, d.annualCost)
+
